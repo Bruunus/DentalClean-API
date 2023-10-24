f56b662513626d9824aa68e9aaad7388b74ce76c
Bruno Fernandes (HSM) <brunus.devbackfront@gmail.com>
2023-10-24 17:10:15 -0300

    Atualização em DeleteController.java

--- src/main/java/br/com/spring/dentalclean/controller/DeleteController.java
+++ src/main/java/br/com/spring/dentalclean/controller/DeleteController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +16,7 @@ import br.com.spring.dentalclean.dto.UsuarioDTO;
 import br.com.spring.dentalclean.service.DeleteService;
 
 @RestController
-<<<<<<< HEAD
-@RequestMapping("")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-=======
 @CrossOrigin(origins = "*")
->>>>>>> ed8e8a12347cb490bdcb80451b99a11885561a9e
 public class DeleteController {
 	
 	
@@ -92,11 +86,7 @@ public class DeleteController {
 	 */
 	
 	@DeleteMapping("/api/delete/dentista")
-<<<<<<< HEAD
-	public ResponseEntity<DentistaDTO> deleteDentistaForIdFalso(@RequestParam("cros") List<Integer> cro) {
-=======
 	public ResponseEntity<DentistaDTO> deleteDentistaForIdFalso(@RequestParam("cro") Integer cro) {
->>>>>>> ed8e8a12347cb490bdcb80451b99a11885561a9e
 		
 		deleteService.deleteDentistaPorCroFalse(cro);
 		
